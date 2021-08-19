f34a0bae682206c71accd5514f0b2425edca2346
gorakhjoshi <>
2021-08-19 13:30:39 +0000

    strict mode

---
+++ 1-strict-mode/app.js
@@ -0,0 +1,16 @@
+// In order to write secure JS code
+'use strict';
+// very first statement in JS. Code above won't be activated.
+//creates visible error in console.
+
+let lockdownSituations = false;
+const canDrive = true;
+
+// if (canDrive) lockdownSituation = true;
+if (canDrive) lockdownSituations = true;
+if (lockdownSituations) console.log("I can't drive");
+
+// const feature = 'Clean';
+// console.log(feature);
+// const private = 'Number';
+const myPrivate = 'Number';
